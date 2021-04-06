d6d415446fad68543952bbb131e4a640f6a0bd12
forbis <32932288+forbis@users.noreply.github.com>
2021-04-06 20:53:55 +0800

    Delete March directory

--- March/1332-Remove Palindromic Subsequences/writeup.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int removePalindromeSub(string s) {
-        string cmp(s.rbegin(), s.rend());
-        if(s.empty()) { return 0; }       
-        else if(!s.compare(cmp)) { return 1; }
-        else { return 2; }
-    }
-};
