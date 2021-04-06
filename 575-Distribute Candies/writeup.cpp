7cd5cdf23ceedece3e5bf1a97ac4b4ab7e5826ac
forbis <32932288+forbis@users.noreply.github.com>
2021-04-06 20:52:35 +0800

    Delete 575-Distribute Candies directory

--- 575-Distribute Candies/writeup.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
-public:
-    int distributeCandies(vector<int>& candyType) {
-        set<int> ans(candyType.begin(), candyType.end());
-        return min((candyType.size() / 2), ans.size());
-    }
-};
